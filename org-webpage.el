4100b947818123a8701b055937ab7214be01e1e2
Feng Shu <tumashu@163.com>
2015-08-21 23:02:59 +0800

    update documents

--- org-webpage.el
+++ org-webpage.el
@@ -28,25 +28,26 @@
 ;; 1. Sources:   https://github.com/tumashu/org-webpage
 ;; 2. Documents: http://tumashu.github.io/org-webpage
 
-;; org-webpage is a fork of Kelvin H's org-page (https://github.com/kelvinh/org-page),
-;; and provides similar features as org-page, the main differents is as follow:
+;; org-webpage is a static site generator based on [[http://orgmode.org/][org-mode]],
+;; which code derived from Kelvin H's [[https://github.com/kelvinh/org-page][org-page]].
 
-;; 1. org-page focus on personal blog while org-webpage is main used to
-;;    generate small project website.
-;; 2. org-page use many customizable variables to configure org-page
-;;    while org-website use an `org-publish-project-alist' style
-;;    alist to adjust org-website's behaver.
+;; The main differents of two projects are as follow:
 
-;;    Managing multi-site configs in an emacs session with org-website is more
-;;    simple than with org-page.
-;; 3. org-website can deal with "increment" or "inherit" themes.
-;;
-;;    A "increment" theme is a mod theme which only include changed template,
-;;    css and other files, the files same with base theme doesn't include.
+;; 1. org-webpage's core don't hard code git, its process is different from org-page, which like:
+;;    #+BEGIN_EXAMPLE
+;;    [ org files ] --( export )--> [ html files ] -------
+;;          \                                             \
+;;           \--------(generate)--> [ Upload bash script] ---> ( git repos )--\
+;;                                         \                                   \
+;;                                          \------------------------------------( upload )---> remote
 
-;;    org-webpage autosearch the same files from base theme when use "increment"
-;;    theme.
+;;    #+END_EXAMPLE
+;; 2. org-webpage's default config is `org-publish-project-alist' style alist,
+;;    which can manage multi-site configs in an emacs session easily.
+;; 3. org-website find theme-files from a *themes-list* in sequence and same theme-file
+;;    first found will be used. User can set *fallback theme* with the help of this feature.
 ;; 4. org-website include a tiny emacs web server, which can be used to test publish.
+;; 5. ...
 
 
 ;;; Code:
